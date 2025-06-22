03c2b8bfde53066aca11f368c7a1a22efef2f85f
LeticiaParreiras <ticiaxp@gmail.com>
2025-06-22 12:50:31 -0300

    finished playlist page

---
+++ public/src/scripts/playlist.js
@@ -0,0 +1,47 @@
+const text = document.querySelector(".text")
+const link = document.querySelector(".link")
+const img = document.querySelector(".img")
+
+function show( index){
+
+    switch(index){
+        case (1):
+            text.innerHTML = `
+            <div class="title"><p>X O -</p></div>
+                <div class="inside">
+                    <p>Songs where I feel like I'm a jellyfish underwater</p>
+                    <ol>
+                        <li>Underwater - Chuu</li>
+                        <li>Yume Utsutsu - Lamp</li>
+                        <li>Hatachi No Koi - Lamp</li>
+                        <li>Cool With You - New Jeans</li>
+                        <li>Get Up - New Jeans</li>
+                        <li>Movies - Weyes Blood</li>
+                        <li>TicTac - Capsule</li>
+                        <li>Dorothy - Sulli</li>
+                        <li>Goblin - Sulli</li>
+                        <li>Sing - Slowdive</li>
+                        <li>Pipo Pipo - Serani Poji</li>
+                        <li>tinkerbell is overrated feat. PinkPantheress - Beabadoobee</li>
+                    </ol>
+                </div>
+            `
+            link.innerHTML = `
+              <div class="title"><p>X O -</p></div>
+
+           <iframe width="100%" height="260px" src="https://www.youtube.com/embed/videoseries?si=jL9RtDVsiV3YX7nx&amp;list=PLZoaSVF3EZTZdV1yMH6QlIkXEFYTeGozH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            `
+            img.innerHTML = `
+                 <div class="title"><p>X O -</p></div>
+        <img src="src/img/jelly.jpg" alt="">
+            `
+            break
+        case(2):
+        text.innerHTML = `OIeeee`
+    }
+    text.style.display = "block"
+    link.style.display = "block"
+    img.style.display = "block"
+    
+
+}
